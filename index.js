f15146855528c34b793280d5802c48d0a86f6bfc
isaacs <i@izs.me>
2017-09-10 21:41:42 -0700

    write streams

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 'use strict'
 const MiniPass = require('minipass')
+const EE = require('events').EventEmitter
 const fs = require('fs')
 
+// for writev
+const binding = process.binding('fs')
+const writeBuffers = binding.writeBuffers
+const FSReqWrap = binding.FSReqWrap
+
 const _close = Symbol('_close')
+const _ended = Symbol('_ended')
 const _fd = Symbol('_fd')
+const _finished = Symbol('_finished')
+const _flags = Symbol('_flags')
+const _flush = Symbol('_flush')
 const _handleChunk = Symbol('_handleChunk')
 const _makeBuf = Symbol('_makeBuf')
+const _mode = Symbol('_mode')
+const _needDrain = Symbol('_needDrain')
 const _onerror = Symbol('_onerror')
 const _onopen = Symbol('_onopen')
 const _onread = Symbol('_onread')
+const _onwrite = Symbol('_onwrite')
 const _open = Symbol('_open')
 const _path = Symbol('_path')
+const _queue = Symbol('_queue')
 const _read = Symbol('_read')
 const _readSize = Symbol('_readSize')
 const _reading = Symbol('_reading')
 const _remain = Symbol('_remain')
 const _size = Symbol('_size')
+const _write = Symbol('_write')
+const _writing = Symbol('_writing')
 
-// XXX support passing in file size to avoid the extra 0b read
 class ReadStream extends MiniPass {
   constructor (path, opt) {
     opt = opt || {}
@@ -173,12 +188,150 @@ class ReadStreamSync extends ReadStream {
   }
 }
 
-class WriteStream extends MiniPass {
+class WriteStream extends EE {
+  constructor (path, opt) {
+    opt = opt || {}
+    super(opt)
+    this.readable = false
+    this[_writing] = false
+    this[_ended] = false
+    this[_needDrain] = false
+    this[_queue] = []
+    this[_path] = path
+    this[_fd] = typeof opt.fd === 'number' ? opt.fd : null
+    this[_flags] = opt.flags === undefined ? 'w' : opt.flags
+    this[_mode] = opt.mode === undefined ? 0o666 : opt.mode
+    if (typeof this[_fd] !== 'number')
+      this[_open]()
+  }
 
+  [_onerror] (er) {
+    if (typeof this[_fd] === 'number')
+      this[_close]()
+    this[_writing] = true
+    this.emit('error', er)
+  }
+
+  [_open] () {
+    fs.open(this[_path], this[_flags], this[_mode],
+      (er, fd) => this[_onopen](er, fd))
+  }
+
+  [_onopen] (er, fd) {
+    if (er)
+      this[_onerror](er)
+    else {
+      this[_fd] = fd
+      this.emit('open', fd)
+      this[_flush]()
+    }
+  }
+
+  end (buf, enc) {
+    if (buf)
+      this.write(buf, enc)
+
+    this[_ended] = true
+
+    // synthetic after-write logic, where drain/finish live
+    if (!this[_writing] && !this[_queue].length &&
+        typeof this[_fd] === 'number')
+      this[_onwrite](null, 0)
+  }
+
+  write (buf, enc) {
+    if (typeof buf === 'string')
+      buf = new Buffer(buf, enc)
+
+    if (this[_ended]) {
+      this.emit('error', new Error('write() after end()'))
+      return false
+    }
+
+    if (this[_fd] === null || this[_writing] || this[_queue].length) {
+      this[_queue].push(buf)
+      this[_needDrain] = true
+      return false
+    }
+
+    this[_writing] = true
+    this[_write](buf)
+    return true
+  }
+
+  [_write] (buf) {
+    fs.write(this[_fd], buf, 0, buf.length, null, er =>
+      this[_onwrite](er))
+  }
+
+  [_onwrite] (er) {
+    if (er)
+      this[_onerror](er)
+    else {
+      if (this[_queue].length)
+        this[_flush]()
+      else {
+        this[_writing] = false
+
+        if (this[_ended] && !this[_finished]) {
+          this[_finished] = true
+          this[_close]()
+          this.emit('finish')
+        } else if (this[_needDrain]) {
+          this[_needDrain] = false
+          this.emit('drain')
+        }
+      }
+    }
+  }
+
+  [_flush] () {
+    if (this[_queue].length === 0) {
+      if (this[_ended])
+        this[_onwrite](null, 0)
+    } else if (this[_queue].length === 1)
+      this[_write](this[_queue].pop())
+    else {
+      const iovec = this[_queue]
+      this[_queue] = []
+      writev(this[_fd], iovec, null, er => this[_onwrite](er))
+    }
+  }
+
+  [_close] () {
+    fs.close(this[_fd], _ => _)
+    this[_fd] = null
+  }
 }
 
 class WriteStreamSync extends WriteStream {
+  [_open] () {
+    this[_onopen](null,
+      fs.openSync(this[_path], this[_flags], this[_mode]))
+  }
 
+  [_close] () {
+    try {
+      fs.closeSync(this[_fd])
+    } catch (er) {}
+    this[_fd] = null
+  }
+
+  [_write] (buf) {
+    try {
+      this[_onwrite](null,
+        fs.writeSync(this[_fd], buf, 0, buf.length, null))
+    } catch (er) {
+      this[_onwrite](er, 0)
+    }
+  }
+}
+
+const writev = (fd, iovec, pos, cb) => {
+  const done = er => cb(er, iovec)
+  const req = new FSReqWrap()
+  req.oncomplete = done
+  binding.writeBuffers(fd, iovec, pos, req)
 }
 
 exports.ReadStream = ReadStream
---
+++ test/write.js
@@ -0,0 +1,318 @@
+'use strict'
+
+const t = require('tap')
+const fsm = require('../')
+const fs = require('fs')
+const EE = require('events').EventEmitter
+const mutateFS = require('mutate-fs')
+
+t.test('basic write', t => {
+  const p = __dirname + '/basic-write'
+
+  const check = t => {
+    t.equal(fs.readFileSync(p, 'utf8'), 'ok')
+    fs.unlinkSync(p)
+    t.end()
+  }
+
+  t.test('sync', t => {
+    new fsm.WriteStreamSync(p).end('ok')
+    check(t)
+  })
+
+  t.test('async', t => {
+    const s = new fsm.WriteStream(p)
+    s.end('ok')
+    s.on('finish', _ => check(t))
+  })
+
+  t.end()
+})
+
+t.test('write then end', t => {
+  const p = __dirname + '/write-then-end'
+
+  const check = t => {
+    t.equal(fs.readFileSync(p, 'utf8'), 'okend')
+    fs.unlinkSync(p)
+    t.end()
+  }
+
+  t.test('sync', t => {
+    const s = new fsm.WriteStreamSync(p)
+    s.write('ok')
+    s.end('end')
+    check(t)
+  })
+
+  t.test('async', t => {
+    const s = new fsm.WriteStream(p)
+    s.write('ok')
+    s.end('end')
+    s.on('finish', _ => check(t))
+  })
+
+  t.end()
+})
+
+t.test('multiple writes', t => {
+  const p = __dirname + '/multiple-writes'
+
+  const check = t => {
+    t.equal(fs.readFileSync(p, 'utf8'), 'abcdefghijklmnop')
+    fs.unlinkSync(p)
+    t.end()
+  }
+
+  t.test('sync', t => {
+    const s = new fsm.WriteStreamSync(p)
+    s.write('a')
+    s.write('b')
+    s.write('c')
+    s.write('d')
+    s.write('e')
+    s.write('f')
+    s.write(new Buffer('676869', 'hex'))
+    s.write('jklm')
+    s.write(new Buffer('nop'))
+    s.end()
+    check(t)
+  })
+
+  t.test('async', t => {
+    const s = new fsm.WriteStream(p)
+    s.write('a')
+    s.write('b')
+    s.write('c')
+    s.write('d')
+    s.write('e')
+    s.write('f')
+    s.write(new Buffer('676869', 'hex'))
+    s.write('jklm')
+    s.write(new Buffer('nop'))
+    s.end()
+    s.on('finish', _ => check(t))
+  })
+
+  t.test('async after open', t => {
+    const s = new fsm.WriteStream(p)
+    s.on('open', fd => {
+      t.isa(fd, 'number')
+      t.ok(s.write('a'))
+      t.notOk(s.write('b'))
+      t.notOk(s.write('c'))
+      t.notOk(s.write('d'))
+      t.notOk(s.write('e'))
+      t.notOk(s.write('f'))
+      t.notOk(s.write(new Buffer('676869', 'hex')))
+      t.notOk(s.write('jklm'))
+      t.notOk(s.write(new Buffer('nop')))
+      s.end()
+      s.on('finish', _ => check(t))
+    })
+  })
+
+  t.test('async after open, drains', t => {
+    const s = new fsm.WriteStream(p)
+    s.on('open', fd => {
+      t.isa(fd, 'number')
+      t.ok(s.write('a'))
+      t.notOk(s.write('b'))
+      s.once('drain', _ => {
+        t.ok(s.write('c'))
+        t.notOk(s.write('d'))
+        t.notOk(s.write('e'))
+        s.once('drain', _ => {
+          t.ok(s.write('f'))
+          t.notOk(s.write(new Buffer('676869', 'hex')))
+          t.notOk(s.write('jklm'))
+          t.notOk(s.write(new Buffer('nop')))
+          s.once('drain', _ => s.end())
+        })
+      })
+      s.on('finish', _ => check(t))
+    })
+  })
+  t.end()
+})
+
+t.test('flags', t => {
+  const p = __dirname + '/flags'
+
+  const check = t => {
+    t.equal(fs.readFileSync(p, 'utf8'), 'ok')
+    fs.unlinkSync(p)
+    t.end()
+  }
+
+  t.test('sync', t => {
+    new fsm.WriteStreamSync(p, { flags: 'w+' }).end('ok')
+    check(t)
+  })
+
+  t.test('async', t => {
+    const s = new fsm.WriteStream(p, { flags: 'w+' })
+    s.end('ok')
+    s.on('finish', _ => check(t))
+  })
+
+  t.end()
+})
+
+t.test('mode', t => {
+  const p = __dirname + '/mode'
+
+  const check = t => {
+    t.equal(fs.readFileSync(p, 'utf8'), 'ok')
+    t.equal(fs.statSync(p).mode & 0o777, 0o700)
+    fs.unlinkSync(p)
+    t.end()
+  }
+
+  t.test('sync', t => {
+    new fsm.WriteStreamSync(p, { mode: 0o700 }).end('ok')
+    check(t)
+  })
+
+  t.test('async', t => {
+    const s = new fsm.WriteStream(p, { mode: 0o700 })
+    s.end('ok')
+    s.on('finish', _ => check(t))
+  })
+
+  t.end()
+})
+
+t.test('write after end', t => {
+  const p = __dirname + '/write-after-end'
+
+  const check = t => {
+    t.equal(fs.readFileSync(p, 'utf8'), 'ok')
+    fs.unlinkSync(p)
+    t.end()
+  }
+
+  t.test('sync', t => {
+    const s = new fsm.WriteStreamSync(p, { mode: 0o700 })
+    s.end('ok')
+    t.throws(_ => s.write('626164', 'hex'),
+      new Error('write() after end()'))
+    check(t)
+  })
+
+  t.test('async', t => {
+    const s = new fsm.WriteStream(p, { mode: 0o700 })
+    s.end('ok')
+    s.on('error', e => {
+      t.match(e, new Error('write() after end()'))
+      s.on('finish', _ => check(t))
+    })
+    s.write('626164', 'hex')
+  })
+
+  t.end()
+})
+
+t.test('fd', t => {
+  const p = __dirname + '/fd'
+
+  const check = t => {
+    t.equal(fs.readFileSync(p, 'utf8'), 'ok')
+    fs.unlinkSync(p)
+    t.end()
+  }
+
+  t.test('sync', t => {
+    const fd = fs.openSync(p, 'w')
+    new fsm.WriteStreamSync(p, { fd: fd }).end('ok')
+    check(t)
+  })
+
+  t.test('async', t => {
+    const fd = fs.openSync(p, 'w')
+    const s = new fsm.WriteStream(p, { fd: fd })
+    s.end('ok')
+    s.on('finish', _ => check(t))
+  })
+
+  t.end()
+})
+
+t.test('empty write', t => {
+  const p = __dirname + '/empty-write'
+
+  const check = t => {
+    t.equal(fs.readFileSync(p, 'utf8'), '')
+    fs.unlinkSync(p)
+    t.end()
+  }
+
+  t.test('sync', t => {
+    t.test('empty string', t => {
+      new fsm.WriteStreamSync(p).end('')
+      check(t)
+    })
+    t.test('no chunk to end', t => {
+      new fsm.WriteStreamSync(p).end('')
+      check(t)
+    })
+    t.end()
+  })
+
+  return t.test('async', t => {
+    t.test('immediate', t => {
+      t.test('no chunk to end', t => {
+        const s = new fsm.WriteStream(p)
+        s.end()
+        s.on('finish', _ => check(t))
+      })
+
+      return t.test('empty string', t => {
+        const s = new fsm.WriteStream(p)
+        s.end('')
+        s.on('finish', _ => check(t))
+      })
+    })
+
+    return t.test('end on open', t => {
+      t.test('no chunk to end', t => {
+        const s = new fsm.WriteStream(p)
+        s.on('open', _ => s.end())
+        s.on('finish', _ => check(t))
+      })
+
+      return t.test('empty string', t => {
+        const s = new fsm.WriteStream(p)
+        s.on('open', _ => s.end(''))
+        s.on('finish', _ => check(t))
+      })
+    })
+  })
+})
+
+t.test('fail open', t => {
+  const p = __dirname + '/fail-open'
+  const poop = new Error('poop')
+  t.teardown(mutateFS.fail('open', poop))
+  t.throws(_ => new fsm.WriteStreamSync(p), poop)
+  const str = new fsm.WriteStream(p)
+  str.on('error', er => {
+    t.equal(er, poop)
+    t.end()
+  })
+})
+
+t.test('fail write', t => {
+  const p = __dirname + '/fail-write'
+  const poop = new Error('poop')
+  t.teardown(mutateFS.fail('write', poop))
+
+  t.throws(_ => new fsm.WriteStreamSync(p).write('foo'), poop)
+  const str = new fsm.WriteStream(p)
+  str.write('foo')
+  str.on('error', er => {
+    t.equal(er, poop)
+    fs.unlinkSync(p)
+    t.end()
+  })
+})
